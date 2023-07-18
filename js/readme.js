c2746d2b7b7e8d7da9cd4f198b09325619edba39
mauroluciano <mauroluciano911@gmail.com>
2023-07-18 16:42:39 +0200

    add prompt

---
+++ js/readme.js
@@ -0,0 +1,13 @@
+const firstname = prompt("Inserisci il tuo nome");
+
+const surname = prompt("Inserisci il tuo cognome");
+
+const color = prompt("Inserisci il tuo colore preferito");
+
+color = color + "21";
+
+const generatedInfo = firstname + surname + color;
+
+const welcomeMessage = <h1>Benvenuto ${generatedInfo}</h1>;
+
+document.getElementById("message").innerHTML = welcomeMessage;
